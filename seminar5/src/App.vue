ac45c6a93933d831baa44d35986df38146a13480
Evgeniy Phive <phivedec@gmail.com>
2024-07-01 00:07:08 +0800

    the 5th homework added

---
+++ seminar5/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <main class="center">
+    <h1 class="title">SEMINAR 5. HOMEWORK</h1>
+    <ProductDetailsComp/>
+  </main>
+</template>
+
+<script>
+import ProductDetailsComp from './components/ProductDetailsComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ProductDetailsComp
+  }
+}
+</script>
+
+<style lang="sass">
+*
+  font-family: Arial, Helvetica, sans-serif
+  box-sizing: border-box
+  text-decoration: none
+  padding: 0
+  margin: 0
+.center
+  padding-left: calc(50% - 486px)
+  padding-right: calc(50% - 486px)
+.title
+  margin-top: 32px
+  color: rgba(46, 124, 209, 1)
+</style>
---
+++ seminar5/src/components/ProductDetailsComp.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class='product'>
+    <h2 class='product__name'>{{product.name}}</h2>
+    <p class='product__price'>Price: {{formattedPrice}}</p>
+    <button @click="changeAvailability()" class='product__button'>Сhange availability status</button>
+    <p v-if="product.available" class='product__status product__status-available'>Available</p>
+    <p v-else class='product__status product__status-not-available'>Out of stock</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductDetailsComp',
+  data () {
+    return {
+      product: {
+        name: 'Laptop',
+        price: 500,
+        available: true
+      }
+    }
+  },
+  methods: {
+    changeAvailability () {
+      this.product.available = !this.product.available
+    }
+  },
+  computed: {
+    formattedPrice () {
+      return `$${this.product.price}`
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.product
+    display: flex
+    flex-direction: column
+    justify-content: space-between
+    gap: 24px
+    padding: 24px 16px
+    width: 30%
+    margin-top: 64px
+    border: 1px solid rgba(46, 124, 209, 1)
+    border-radius: 24px
+    transition: all .3s
+    &:hover
+      box-shadow: 8px 8px 8px rgba(46, 46, 46, .1)
+    &__name
+      color: rgba(46, 124, 209, 1)
+      font-size: 32px
+    &__price
+      color: rgba(46, 124, 209, .8)
+      font-size: 20px
+    &__button
+      align-self: center
+      background-color: #fff
+      border: 1px solid rgba(46, 124, 209, 1)
+      border-radius: 16px
+      cursor: pointer
+      width: 90%
+      font-size: 16px
+      color: rgba(46, 124, 209, 1)
+      padding: 12px 8px
+      transition: all .2s
+      &:hover
+        background-color: rgba(46, 124, 209, 1)
+        color: #fff
+    &__status
+      text-align: center
+      font-size: 22px
+      &-available
+        color: rgba(45, 153, 100, .7)
+      &-not-available
+        color: rgba(201, 64, 121, .7)
+</style>
